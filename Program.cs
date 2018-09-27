9eeebe67f30fb275d96add9a1dfe3a0a29683ffc
CaitlinCodes <caiit@me.com>
2018-09-26 21:54:10 -0600

    program cs

--- Program.cs
+++ Program.cs
@@ -3,11 +3,13 @@ using CastleGrimtol.Project;
 
 namespace CastleGrimtol
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-
-        }
+      Console.WriteLine("Welcome, young warrior. You have entered the tunnel of Castle Grimtol. Do you have what it takes to survive?");
+      Game game = new Game();
+      game.StartGame();
     }
+  }
 }
