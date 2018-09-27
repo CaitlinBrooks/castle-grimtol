6880d53bf4583a90250f429a56b63954b625235a
CaitlinCodes <caiit@me.com>
2018-09-26 22:27:30 -0600

    closer to options for Game.cs, still errors

--- Project/models/Game.cs
+++ Project/models/Game.cs
@@ -43,52 +43,43 @@ namespace CastleGrimtol.Project
       }
     }
 
-    public void Go(string direction)
-    {
-      throw new System.NotImplementedException();
-    }
-
-    public void Help()
-    {
-      throw new System.NotImplementedException();
-    }
-
-    public void Inventory()
-    {
-      throw new System.NotImplementedException();
-    }
-
-    public void Look()
-    {
-      throw new System.NotImplementedException();
-    }
-
-    public void Quit()
-    {
-      throw new System.NotImplementedException();
-    }
-
-    public void Reset()
-    {
-      throw new System.NotImplementedException();
-    }
-
     public void Setup()
     {
       throw new System.NotImplementedException();
     }
-
     public void StartGame()
     {
       throw new System.NotImplementedException();
     }
-
+    public void Go(string direction)
+    {
+      throw new System.NotImplementedException();
+    }
+    public void Look()
+    {
+      throw new System.NotImplementedException();
+    }
+    public void Inventory()
+    {
+      throw new System.NotImplementedException();
+    }
     public void TakeItem(string itemName)
     {
       throw new System.NotImplementedException();
     }
-
     public void UseItem(string itemName)
+    {
+      throw new System.NotImplementedException();
+    }
+    public void Quit()
+    {
+      throw new System.NotImplementedException();
+    }
+    public void Help()
+    {
+      throw new System.NotImplementedException();
+    }
+    public void Reset()
     {
       throw new System.NotImplementedException();
     }
